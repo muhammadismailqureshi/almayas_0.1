2032574d3378aefaa1d438875db8dfef91abb7b2
almayas_0.1 <admin@almayas.com.pk>
2023-03-20 08:54:37 +0500

    adding redux to the product screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,11 @@
 import { applyMiddleware, configureStore, compose, combineReducers } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
-import { productListReducer } from './reducers/productReducers';
+import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 
 const initialState ={};
 const reducer = combineReducers({
     productList: productListReducer,
+    productDetails: productDetailsReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
